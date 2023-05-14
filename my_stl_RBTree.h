3c5e5738f4b2844905cb472751ba8e2b15df352f
Yukikaze-1 <wuquan5814675@163.com>
2023-05-14 09:27:22 +0800

    1st commit

--- my_stl_RBTree.h
+++ my_stl_RBTree.h
@@ -1,4 +1,35 @@
 #ifndef __MY_STL_RBTREE_H__
 #define __MY_STL_RBTREE_H__ 1
 
+#include "my_stl_algorithm.h"
+#include "my_memory.h"
+
+
+template<class T> 
+struct tb_tree_node_base{
+
+};
+
+template<class T> 
+struct tb_tree_node : public tb_tree_node_base<T>{
+
+};
+
+template<class T> 
+struct tb_tree_base_iterator{
+
+};
+
+template<class T> 
+struct tb_tree_iterator : public tb_tree_base_iterator<T>{
+
+};
+
+
+//rbtree definition
+template <class Value,class Key,class KeyOfValue>
+class _rb_tree{
+
+};
+
 #endif
